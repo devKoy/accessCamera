67152e1e9991f9146337c6e0f9187b9fd7786a54
devKoy <83859566+devKoy@users.noreply.github.com>
2022-05-30 19:43:25 +0800

    Add files via upload

---
+++ res/script/camera.js
@@ -0,0 +1,176 @@
+
+
+//Get Prediction from Model
+function uploadFile() {
+	
+	var formData = new FormData(); 
+	var fileInput = document.getElementById('fileInput'); 
+	console.log(fileInput.files[0]);
+	if (fileInput.files[0]){
+		formData.append("classified_id", 2);
+		formData.append("file", fileInput.files[0]); 					
+		axios({
+			method: 'post',
+			url: 'https://bananaapi.herokuapp.com/predict', 
+			data: formData,
+			headers: { 
+			  'Accept': 'application/json',
+			  'Content-Type': 'multipart/form-data' },
+		}).then(function(response) {
+			$("#result").text(response.data);
+			console.log(response);
+		}) .catch(function(response) {
+			$("#result").text(response.data);
+			console.error(response);
+		});
+	} 
+}
+
+// Capture Image
+function take_snapshot() {
+ 
+	// take snapshot and get image data
+	Webcam.snap( function(data_uri) {
+		// display results in page
+		document.getElementById('results').innerHTML = 
+		 '<img src="'+data_uri+'"/>';
+	 } );
+ }
+
+//  - --------------------CAMERA---------------------
+
+	// The width and height of the captured photo. We will set the
+	// width to the value defined here, but the height will be
+	// calculated based on the aspect ratio of the input stream.
+  
+	var width = window.innerWidth;    // We will scale the photo width to this
+	var height = window.innerHeight;     // This will be computed based on the input stream
+  
+	// |streaming| indicates whether or not we're currently streaming
+	// video from the camera. Obviously, we start at false.
+  
+	var streaming = false;
+  
+	// The various HTML elements we need to configure or control. These
+	// will be set by the startup() function.
+  
+	var video = document.getElementById('video');
+	var canvas = null;
+	var photo = null;
+	var startbutton = null;
+  
+	function showViewLiveResultButton() {
+	  if (window.self !== window.top) {
+		// Ensure that if our document is in a frame, we get the user
+		// to first open it in its own tab or window. Otherwise, it
+		// won't be able to request permission for camera access.
+		document.querySelector(".contentarea").remove();
+		const button = document.createElement("button");
+		button.textContent = "View live result of the example code above";
+		document.body.append(button);
+		button.addEventListener('click', () => window.open(location.href));
+		return true;
+	  }
+	  return false;
+	}
+  
+	function startup() {
+    
+	  if (showViewLiveResultButton()) { return; }
+	 
+	  canvas = document.getElementById('canvas');
+	  photo = document.getElementById('photo');
+	  clearButton = document.getElementById('clear');
+	  startbutton = document.getElementById('scanner');
+  
+	  navigator.mediaDevices.getUserMedia({video: {
+		  facingMode: "environment"
+	  }, audio: false})
+	  .then(function(stream) {
+		video.srcObject = stream;
+		video.play();
+	  })
+	  .catch(function(err) {
+		console.log("An error occurred: " + err);
+	  });
+  
+  
+	  startbutton.addEventListener('click', function(ev){
+        if (menu_indicator.classList.contains("cam-indicator")){
+            takepicture();
+		    ev.preventDefault();
+        }
+	  }, false);
+	  clearButton.addEventListener('click', function(ev){
+		let div = document.getElementById("cont");
+		div.style.display = 'flex';
+		clearphoto();
+		ev.preventDefault();
+	  }, false);
+	 
+	}
+    
+    function killCam(){
+        video.pause();
+        try{
+            video.srcObject.getTracks().forEach(track => track.stop());    
+        }catch(err){
+            console.log("CAMERA already dead")
+        }
+        
+    }
+	// Fill the photo with an indication that none has been
+	// captured.
+  
+	function clearphoto() {
+	  canvas.style.display = 'none';
+	}
+  
+	// Capture a photo by fetching the current contents of the video
+	// and drawing it into a canvas, then converting that to a PNG
+	// format data URL. By drawing it on an offscreen canvas and then
+	// drawing that to the screen, we can change its size and/or apply
+	// other changes before drawing it.
+  
+	function takepicture() {
+	  var context = canvas.getContext('2d');
+	  if (width && height) {
+		canvas.width = width;
+		canvas.height = height;
+		context.drawImage(video, 0, 0, width, height);
+		canvas.style.display = 'inline-block';
+		var data = canvas.toDataURL('image/jpeg', 1.0);
+		let div = document.getElementById("cont");
+		canvas.toBlob((blob) => {
+			let file = new File([blob], "fileName.jpg", { type: "image/jpeg" })
+			let result = document.getElementById('pred-res');
+			var formData = new FormData(); 
+			formData.append("classified_id", 2);
+			formData.append("file", file); 					
+			$("#pred").attr("src",file);
+			axios({
+				method: 'post',
+				url: 'https://bananaapi.herokuapp.com/predict', 
+				data: formData,
+				headers: { 
+				'Accept': 'application/json',
+				'Content-Type': 'multipart/form-data' },
+			}).then(function(response) {
+				$("#pred-res").text(response.data);
+				console.log(response);
+			}) .catch(function(response) {
+				$("#pred-res").text(response.data);
+				console.error(response);
+			});
+		}, 'image/jpeg');
+		
+	  } else {
+		clearphoto();
+	  }
+	}
+	
+	// Set up our event listener to run the startup process
+	// once loading is complete.
+	
+
+  
---
+++ res/script/nav-bar.js
@@ -0,0 +1,83 @@
+var menu_bar = document.querySelector('.sc-bottom-bar');
+var menu_item = document.querySelectorAll('.sc-menu-item');
+var menu_indicator = document.querySelector('.sc-nav-indicator');
+var menu_current_item = document.querySelector('.sc-current');
+var menu_position;
+var nav_title = document.getElementsByClassName('nav-title');
+
+var home = document.getElementById('home');
+var community = document.getElementById('community');
+var scanner = document.getElementById('scanner-container');
+var almanac = document.getElementById('almanac');
+var profile  = document.getElementById('profile');
+
+menu_position = menu_current_item.offsetLeft - 50;
+menu_indicator.style.left = menu_position + "px";
+menu_bar.style.backgroundPosition = menu_position-8 + 'px';
+menu_item.forEach(
+  function(select_menu_item){
+    select_menu_item.addEventListener('click', function(e){
+      e.preventDefault();
+      menu_position = this.offsetLeft - 50;
+      menu_indicator.style.left = menu_position + "px";
+      menu_bar.style.backgroundPosition = menu_position-8 + 'px';
+      [...select_menu_item.parentElement.children].forEach(
+        sibling => {
+          sibling.classList.remove('sc-current');
+        })
+      select_menu_item.classList.add('sc-current');
+      change_scan_color();
+      for(i=0; i < nav_title.length; i++){nav_title[i].style.display = 'block'; nav_title[i].classList.remove('hidden')}
+      this.children[1].style.display = "none";
+      var current = this.children[1].innerText;
+      changeActivity(current)
+    });
+  }
+)
+
+function change_scan_color(){
+    var scan = document.getElementById('scanner');
+    var item = document.getElementById('nav-scan');
+    if(item.classList.contains("sc-current")){
+        scan.classList.add("scan-selected")  
+    }else{
+        scan.classList.remove("scan-selected")  
+    }
+}
+
+function changeActivity(current) {
+  menu_indicator.classList.remove("cam-indicator");
+    if(current == "home" || current == "Home"){
+        home.style.display = "flex";
+        scanner.style.display = "none";
+        community.style.display = "none";
+        almanac.style.display = "none";
+        profile.style.display = "none";
+    }else if(current == "scan" || current == "Scan"){
+        home.style.display = "none";
+        scanner.style.display = "flex";
+        menu_indicator.classList.add("cam-indicator");
+        community.style.display = "none";
+        almanac.style.display = "none";
+        profile.style.display = "none";
+    }else if(current == "community" || current == "Community"){
+        home.style.display = "none";
+        scanner.style.display = "none";
+        community.style.display = "flex";
+        almanac.style.display = "none";
+        profile.style.display = "none";
+    }else if(current == "almanac" || current == "Almanac"){
+        home.style.display = "none";
+        scanner.style.display = "none";
+        community.style.display = "none";
+        almanac.style.display = "flex";
+        profile.style.display = "none";
+    }else if(current == "me" || current == "Me"){
+        home.style.display = "none";
+        scanner.style.display = "none";
+        community.style.display = "none";
+        almanac.style.display = "none";
+        profile.style.display = "flex";
+    }
+      
+}
