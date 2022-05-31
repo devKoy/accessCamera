9b69c39face89b0d5d9e8b961328a23e844d8b9c
devKoy <83859566+devKoy@users.noreply.github.com>
2022-05-31 22:57:35 +0800

    Add files via upload

---
+++ res/script/map-loader.js
@@ -0,0 +1,57 @@
+var camsur = {
+    "long": 123.323891,
+    "lat":  13.546718
+}
+
+var user = {
+    "long": 0,
+    "lat": 0
+}
+
+var map = L.map('maps').setView([camsur.lat, camsur.long], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+setTimeout(function(){
+   map.invalidateSize(); 
+}, 100);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+function getLocationANDCloseResult() {
+    $("#previewImage").css("display", "none");
+    $("#canvas").css("display", "none");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+}
+
+function showPosition(position) {
+   
+    var res = document.getElementById("res-api").innerText;
+    var resMark = ""
+    if (res.includes("No leaf Detected")){return} else
+    if (res.includes("cordana")){resMark = "Cordana Leaf Spot"} else
+    if (res.includes("healthy")){resMark = "None"} else
+    if (res.includes("sigatoka")){resMark = "Sigatoka Leaf Spot"} else
+    if (res.includes("pestalotiopsis")){resMark = "Pestalotiopsis Leaf Spot"}
+    
+    if(resMark != ""){
+        var marker = L.marker([position.coords.latitude ,
+            position.coords.longitude
+        ]).addTo(map);
+        marker.bindPopup("<b>PLANT:</b> BANANA <br><b>DISEASE:</b> "+resMark).openPopup();
+    }
+    map.invalidateSize()
+    
+}
+
--- res/script/nav-bar.js
+++ res/script/nav-bar.js
@@ -10,17 +10,18 @@ var community = document.getElementById('community');
 var scanner = document.getElementById('scanner-container');
 var almanac = document.getElementById('almanac');
 var profile  = document.getElementById('profile');
+var map = document.getElementById('maps');
 
 menu_position = menu_current_item.offsetLeft - 55;
 menu_indicator.style.left = menu_position + "px";
-menu_bar.style.backgroundPosition = menu_position-0 + 'px';
+menu_bar.style.backgroundPosition = menu_position-10 + 'px';
 menu_item.forEach(
   function(select_menu_item){
     select_menu_item.addEventListener('click', function(e){
       e.preventDefault();
       menu_position = this.offsetLeft - 55;
       menu_indicator.style.left = menu_position + "px";
-      menu_bar.style.backgroundPosition = menu_position-0 + 'px';
+      menu_bar.style.backgroundPosition = menu_position-10 + 'px';
       [...select_menu_item.parentElement.children].forEach(
         sibling => {
           sibling.classList.remove('sc-current');
@@ -47,24 +48,35 @@ function change_scan_color(){
 
 function changeActivity(current) {
   menu_indicator.classList.remove("cam-indicator");
+  
+    var home = document.getElementById('home');
+    var community = document.getElementById('community');
+    var scanner = document.getElementById('scanner-container');
+    var almanac = document.getElementById('almanac');
+    var profile  = document.getElementById('profile');
+    var map = document.getElementById('maps');
+
     if(current == "home" || current == "Home"){
         home.style.display = "flex";
         scanner.style.display = "none";
         community.style.display = "none";
         almanac.style.display = "none";
         profile.style.display = "none";
+        map.style.display = "none";
     }else if(current == "scan" || current == "Scan"){
         home.style.display = "none";
         scanner.style.display = "flex";
         menu_indicator.classList.add("cam-indicator");
         community.style.display = "none";
         almanac.style.display = "none";
+        map.style.display = "none";
         profile.style.display = "none";
     }else if(current == "community" || current == "Community"){
         home.style.display = "none";
         scanner.style.display = "none";
         community.style.display = "flex";
         almanac.style.display = "none";
+        map.style.display = "none";
         profile.style.display = "none";
     }else if(current == "almanac" || current == "Almanac"){
         home.style.display = "none";
@@ -72,12 +84,20 @@ function changeActivity(current) {
         community.style.display = "none";
         almanac.style.display = "flex";
         profile.style.display = "none";
+        map.style.display = "none";
     }else if(current == "me" || current == "Me"){
         home.style.display = "none";
         scanner.style.display = "none";
         community.style.display = "none";
         almanac.style.display = "none";
         profile.style.display = "flex";
+        map.style.display = "none";
+    }else if(current == "Map"){
+        home.style.display = "none";
+        scanner.style.display = "none";
+        community.style.display = "none";
+        almanac.style.display = "none";
+        profile.style.display = "none";
+        map.style.display = "flex";
     }
-      
 }
